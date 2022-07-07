0e40bb653a130f45d301e9c676fbf423f5347fde
hp <hp@LAPTOP-D45J9M0C>
2022-07-07 14:18:31 +0530

    initial commit

---
+++ PageObjectModelTest/src/POMExamples/Firstdemotest.java
@@ -0,0 +1,23 @@
+package POMExamples;
+
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Firstdemotest {
+	@Test
+	public void Takescreenshot() throws IOException, InterruptedException {
+	  
+
+	WebDriver driver= new ChromeDriver(); 
+
+	driver.get("https://www.amazon.in/");
+
+	driver.manage().window().maximize();
+
+	driver.findElement(By.xpath("/html/body/div[1]/header/div/div[5]/div[2]/div/div/a[1]")).click();
+}
+}
